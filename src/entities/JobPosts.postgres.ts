bd572d767328d68ec24dc9eaae5ec47db5cab13a
Tsaikkari <trospe@gmx.de>
2020-11-24 14:15:18 +0100

    Adding JobPost entity

---
+++ src/entities/JobPosts.postgres.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  OneToMany,
+  ManyToMany,
+  JoinTable,
+  JoinColumn,
+} from 'typeorm'
+
+//import Company from './Company.postgres.ts'
+//import Skill from './Skills.postgres.ts'
+
+@Entity()
+export default class JobPost extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number
+
+  @Column()
+  title!: string
+
+  @Column()
+  jobDescription!: string
+
+  @Column()
+  seniority!: string
+
+  // @ManyToMany(type => Skill) @JoinTable()
+  // details: Skill
+
+  // @OneToMany(type => Company) @JoinColumn()
+  // details: Company
+
+  static getJobPostByTitle(title: string) {
+    return this.find({ where: { title: title } })
+  }
+}
