1a5466fbcc63720ca3b7a00993e9bcc95387808a
Tsaikkari <trospe@gmx.de>
2020-11-25 23:38:33 +0100

    Empty employer controller

--- src/routers/employer.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-import { localLogin } from '../controllers/employerLogin'
-
-const router = express.Router()
-
-router.post('/login', localLogin)
-
-export default router
