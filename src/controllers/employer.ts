07e699f146a296eea4a64fe0668576cd5f0274b3
Dilip Poudel <dilippoudel578@gmail.com>
2020-11-30 21:33:27 +0200

    progress on deleteing jobpost

--- src/controllers/employer.ts
+++ src/controllers/employer.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 import bcrypt from 'bcrypt'
 import passport from 'passport'
-
 import {
   NotFoundError,
   UnauthorizedError,
@@ -89,5 +88,23 @@ export const createJobPost = async (
     res.json({ message: 'Posted' })
   } catch (error) {
     next(new InternalServerError(error.message))
+  }
+}
+
+export const deleteJobPostbyId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const id = parseInt(req.params.id)
+    const isJobFound = JobPost.findOne(id)
+    if (!isJobFound) {
+      return next(new NotFoundError('Job is no more available'))
+    }
+
+    res.json({ message: 'success' })
+  } catch (error) {
+    console.log(error)
   }
 }
