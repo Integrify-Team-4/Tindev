4ce06e176c580800a4539fb2c2173e1913519d18
Dilip Poudel <dilippoudel578@gmail.com>
2020-12-07 17:16:51 +0200

    authentication added

--- src/passport/config.ts
+++ src/passport/config.ts
@@ -1,11 +1,12 @@
 import passportLocal from 'passport-local'
 import bcrypt from 'bcrypt'
+import passportJWT, { ExtractJwt } from 'passport-jwt'
 
 import Employer from '../entities/Employer.postgres'
 import Credential from '../entities/Credential.postgres'
 
 const LocalStrategy = passportLocal.Strategy
-
+const JWTStrategy = passportJWT.Strategy
 export const local = new LocalStrategy(
   {
     usernameField: 'email',
@@ -32,5 +33,20 @@ export const local = new LocalStrategy(
     } catch (error) {
       console.log('error', error)
     }
+  }
+)
+
+export const jwt = new JWTStrategy(
+  {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET,
+  },
+  async (jwtPayload, done) => {
+    const { id } = jwtPayload
+    const user = await Credential.findOne(id, {
+      relations: ['jobSeeker', 'employer'],
+    })
+    if (!user) return done(null, false)
+    return done(null, user)
   }
 )
