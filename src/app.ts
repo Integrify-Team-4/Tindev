78e5403ab7438697808b7443e9e533b2813d1d3c
khumair <khawja.umair@gmail.com>
2020-12-14 15:05:00 +0200

    fixed app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+// import socketio from 'socket.io'
+// import http from 'http'
 import compression from 'compression'
 import bodyParser from 'body-parser'
 import lusca from 'lusca'
@@ -8,12 +10,12 @@ import passport from 'passport'
 import apiErrorHandler from './middlewares/apiErrorHandler'
 import apiContentType from './middlewares/apiContentType'
 import responseHandler from './middlewares/responseHandler'
-
 import jobSeekerRouter from './routers/jobSeeker'
 import employerRouter from './routers/employer'
 import skillsRouter from './routers/skills'
 
 import { local, jwt } from './passport/config'
+import { sortedIndex } from 'lodash'
 
 const app = express()
 console.log('IT IS ENVIRONMENT ', ENVIRONMENT)
@@ -32,7 +34,17 @@ app.use(passport.initialize())
 passport.use(local)
 passport.use(jwt)
 
+//** Socket-IO */
+
+// const server = http.createServer(app)
+// const io = socketio(server)
+// io.on('connection', (socket: any) => {
+//   socket.emit('initial', 'connected')
+//   console.log('Connected')
+// })
+
 app.use(responseHandler)
+
 //**All routers here*/
 app.use('/jobSeeker', jobSeekerRouter)
 app.use('/employer', employerRouter)
