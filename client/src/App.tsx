eabddd2ef1360c1ee75105a729e370691e9d4759
Gabriel <olaniyi_g@hotmail.com>
2020-12-14 21:29:22 +0100

    npm start version: bug fixed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <p>Welcome to TinDev</p>
-      <p>Team 4 :) </p>
-      <p>test</p>
-    </div>
-  )
-}
+const App = () => (
+  <div className="App">
+    <p>Welcome to TinDev</p>
+    <p>Team 4 ;)</p>
+  </div>
+)
 
 export default App
