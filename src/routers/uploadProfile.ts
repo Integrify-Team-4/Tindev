c8d7860aabc2a12f1179dafc8cd5dd0d6133ef28
Dilip Poudel <dilippoudel578@gmail.com>
2021-01-19 13:06:51 +0200

    final update

--- src/routers/uploadProfile.ts
+++ src/routers/uploadProfile.ts
@@ -3,7 +3,6 @@ import { uploadImages } from '../controllers/uploader'
 import tokenVerify from '../middlewares/tokenVerify'
 
 const router = express.Router()
-
 router.post('/image', tokenVerify, uploadImages)
 
 export default router
